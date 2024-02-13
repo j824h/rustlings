313c46da5e6be38ada694284ac8aba5d037cff24
Junghyeon Park <j824h@kaist.ac.kr>
2024-02-14 03:39:51 +0900

    Improve conciseness

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -38,9 +38,7 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
-        if !basket.get(&fruit).is_some() {
-            basket.insert(fruit, 1);
-        }
+        basket.entry(fruit).or_insert(1);
     }
 }
 
