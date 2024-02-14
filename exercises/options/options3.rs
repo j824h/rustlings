9dbc7958edc73efb3e0686a8bc0fedcb77bdfd22
Junghyeon Park <j824h@kaist.ac.kr>
2024-02-14 13:59:44 +0900

    Add attemts

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -13,8 +13,9 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
+    // match &y {
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
     y; // Fix without deleting this line.
