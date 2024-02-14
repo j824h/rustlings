ed3c0b4bd18d407bc2631fc2450d5efec5d65f07
Junghyeon Park <j824h@kaist.ac.kr>
2024-02-14 14:12:22 +0900

    Use match instead of if

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -10,9 +10,12 @@ fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // We use the 24-hour system here, so 10PM is a value of 22 and 12AM is a
     // value of 0 The Option output should gracefully handle cases where
     // time_of_day > 23.
-    if time_of_day < 22 { Some(5) }
-    else if time_of_day < 24 { Some(0) }
-    else { None }
+    match &time_of_day {
+        // Exclusive range pattern is experimental.
+        /* .. 22 */ ..=21 => Some(5),
+        /* .. 24 */ ..=23 => Some(0),
+        _ => None,
+    }
 }
 
 #[cfg(test)]
