0cedc48ea91a66c9c64b34d0be4669705a92acc1
Junghyeon Park <j824h@kaist.ac.kr>
2024-03-13 15:22:52 +0900

    Fix formatting

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -3,14 +3,13 @@
 // Execute `rustlings hint macros4` or use the `hint` watch subcommand for a
 // hint.
 
-#[rustfmt::skip]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
     };
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
-    }
+    };
 }
 
 fn main() {
