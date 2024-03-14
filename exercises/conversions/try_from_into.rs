48ec247878bfa6b37d211c7fa94062827cffbe9d
Junghyeon Park <j824h@kaist.ac.kr>
2024-03-14 13:14:49 +0900

    Add an attempt - try_from_into

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,8 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -41,6 +39,19 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let red = tuple.0.try_into();
+        let green = tuple.1.try_into();
+        let blue = tuple.2.try_into();
+
+        if red.is_err() || green.is_err() || blue.is_err() {
+            return Err(Self::Error::IntConversion);
+        }
+
+        let red = red.unwrap();
+        let green = green.unwrap();
+        let blue = blue.unwrap();
+        
+        Ok(Color { red, green, blue })
     }
 }
 
@@ -48,6 +59,19 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let red = arr[0].try_into();
+        let green = arr[1].try_into();
+        let blue = arr[2].try_into();
+
+        if red.is_err() || green.is_err() || blue.is_err() {
+            return Err(Self::Error::IntConversion);
+        }
+
+        let red = red.unwrap();
+        let green = green.unwrap();
+        let blue = blue.unwrap();
+
+        Ok(Color { red, green, blue })
     }
 }
 
@@ -55,6 +79,11 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(Self::Error::BadLen);
+        }
+
+        Color::try_from(<[i16; 3]>::try_from(slice).unwrap())
     }
 }
 
