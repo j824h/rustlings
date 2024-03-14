ae90b4f7b0a296bcb112f618854c46a4dc5f22ea
Junghyeon Park <j824h@kaist.ac.kr>
2024-03-14 12:19:53 +0900

    Add an attempt - from_str

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -52,6 +50,28 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(Self::Err::Empty);
+        }
+
+        let s = s.split(',').collect::<Vec<_>>();
+        if s.len() != 2 {
+            return Err(Self::Err::BadLen);
+        }
+
+        let name = s[0];
+        if name.is_empty() {
+            return Err(Self::Err::NoName);
+        }
+        let name = name.to_string();
+
+        let age = s[1].parse::<usize>();
+        if age.is_err() {
+            return Err(Self::Err::ParseInt(age.unwrap_err()));
+        }
+        let age = age.unwrap();
+
+        Ok(Person { name, age })
     }
 }
 
